1be17a806e90bd866c506a00f42bebcd04dbe59a
jinen <jinenabdelhakdev@gmail.com>
2019-08-21 18:18:33 +0100

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Jinen</h2>
-      <p>
-        Need a developer ? <Link to="/contact">Contact Me.</Link>
-      </p>
     </Layout>
   )
 }
